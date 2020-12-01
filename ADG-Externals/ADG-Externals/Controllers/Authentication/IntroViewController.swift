365b4badb126a74951cd7be6287c3f8ba0baaf23
PRABAL4546 <prabaljitwalia4546@gmail.com>
2020-12-01 20:16:45 +0530

    intro-page-edit

---
+++ ADG-Externals/ADG-Externals/Controllers/Authentication/IntroViewController.swift
@@ -0,0 +1,35 @@
+//
+//  IntroViewController.swift
+//  ADG-Externals
+//
+//  Created by PRABALJIT WALIA on 01/12/20.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+
+    //MARK:-Connections
+    @IBAction func signUpBtn(_ sender: Any) {
+        self.performSegue(withIdentifier: "signupsegue", sender: nil)
+    }
+    
+    @IBAction func loginBtn(_ sender: Any) {
+        self.performSegue(withIdentifier: "loginSegue", sender: nil)
+    }
+    @IBOutlet weak var signUpBtnLabel:UIButton!
+    @IBOutlet weak var logInBtnLabel:UIButton!
+    
+    //MARK:- View Lifecycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        signUpBtnLabel.layer.cornerRadius = 15
+        logInBtnLabel.layer.cornerRadius = 15
+        logInBtnLabel.layer.borderWidth = 1
+        logInBtnLabel.layer.borderColor = UIColor.orange.cgColor
+    }
+    
+}
