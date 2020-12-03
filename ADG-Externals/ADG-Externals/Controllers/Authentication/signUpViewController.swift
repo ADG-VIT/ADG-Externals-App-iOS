392a4f3ca217fc8113dc351bd14a0579a260fa7f
PRABAL4546 <prabaljitwalia4546@gmail.com>
2020-12-03 23:32:01 +0530

    signUp Page layout

--- ADG-Externals/ADG-Externals/Controllers/Authentication/signUpViewController.swift
+++ ADG-Externals/ADG-Externals/Controllers/Authentication/signUpViewController.swift
@@ -2,18 +2,31 @@
 //  signUpViewController.swift
 //  ADG-Externals
 //
-//  Created by Gokul Nair on 30/11/20.
+//  Created by PRABALJIT WALIA on 03/12/20.
 //
 
 import UIKit
 
 class signUpViewController: UIViewController {
+    
 
+    //MARK:- ViewLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        continueButtonLabel.layer.cornerRadius = 10
+        continueButtonLabel.layer.borderWidth = 2
+        continueButtonLabel.layer.borderColor = UIColor.systemOrange.cgColor
+    }
+    //MARK:- IBConnections
+    
+    @IBAction func fullNameField(_ sender: UITextField) {
     }
     
-
+    @IBAction func continueButton(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "signUpToRecruitmentVC", sender: self)
+    }
+    
+    @IBOutlet weak var continueButtonLabel: UIButton!
 }
