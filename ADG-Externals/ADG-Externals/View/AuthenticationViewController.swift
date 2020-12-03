a317f210909600005e51ee28474d4fe0d4c789b9
PRABAL4546 <prabaljitwalia4546@gmail.com>
2020-12-03 22:42:02 +0530

    auth intro

--- ADG-Externals/ADG-Externals/View/AuthenticationViewController.swift
+++ ADG-Externals/ADG-Externals/View/AuthenticationViewController.swift
@@ -2,28 +2,37 @@
 //  AuthenticationViewController.swift
 //  ADG-Externals
 //
-//  Created by Gokul Nair on 03/12/20.
+//  Created by PRABALJIT WALIA on 03/12/20.
 //
 
 import UIKit
 
 class AuthenticationViewController: UIViewController {
-
+    
+    //MARK:-IBConnections
+    @IBOutlet weak var signUpLabel: UIButton!
+    @IBOutlet weak var logInLabel: UIButton!
+    
+    
+    @IBAction func logInButton(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "introToLoginSegue", sender: self)
+    }
+    @IBAction func signUpButton(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "introToSignUpSegue", sender: self)
+    }
+    
+    
+    //MARK:-ViewLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        signUpLabel.layer.cornerRadius = 10
+        logInLabel.layer.cornerRadius = 10
+        logInLabel.layer.borderWidth = 2
+        logInLabel.layer.borderColor = UIColor.systemOrange.cgColor
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
