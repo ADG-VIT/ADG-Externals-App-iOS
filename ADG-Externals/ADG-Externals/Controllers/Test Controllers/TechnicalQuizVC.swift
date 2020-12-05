f35a4ace129790924ac19472f1bac55c8e739394
“gokulnair2001” <gokulnair.2001@gmail.com>
2020-12-05 20:59:08 +0530

    techquiz SB

---
+++ ADG-Externals/ADG-Externals/Controllers/Test Controllers/TechnicalQuizVC.swift
@@ -0,0 +1,49 @@
+//
+//  TechnicalQuizVC.swift
+//  ADG-Externals
+//
+//  Created by Gokul Nair on 05/12/20.
+//
+
+import UIKit
+
+class TechnicalQuizVC: UIViewController {
+
+    @IBOutlet weak var questionNumberLbl: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var choice1: UIButton!
+    @IBOutlet weak var choice2: UIButton!
+    @IBOutlet weak var choice3: UIButton!
+    @IBOutlet weak var choice4: UIButton!
+    
+    @IBOutlet weak var skipButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        applyBorder(button: choice1, RadiusSize: 10, widthSize: 0.5, color: #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1))
+        applyBorder(button: choice2, RadiusSize: 10, widthSize: 0.5, color: #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1))
+        applyBorder(button: choice3, RadiusSize: 10, widthSize: 0.5, color: #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1))
+        applyBorder(button: choice4, RadiusSize: 10, widthSize: 0.5, color: #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1))
+       
+        applyBorder(button: skipButton, RadiusSize: 5, widthSize: 2, color: UIColor.systemOrange.cgColor)
+        applyBorder(button: nextButton, RadiusSize: 5, widthSize: 2, color: UIColor.systemOrange.cgColor)
+    }
+    
+
+  
+}
+
+
+//MARK:- UI Implementation Methods
+
+extension TechnicalQuizVC{
+    func applyBorder(button: UIButton, RadiusSize: CGFloat,widthSize: CGFloat, color: CGColor){
+        
+        button.layer.cornerRadius = RadiusSize
+        button.layer.borderWidth = widthSize
+        button.layer.borderColor = color
+    }
+}
