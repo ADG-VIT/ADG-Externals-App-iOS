664fc3464b6b493613e560f75a6ecc89f6f80f7c
PRABAL4546 <prabaljitwalia4546@gmail.com>
2020-12-07 16:36:25 +0530

    signUp working

--- ADG-Externals/ADG-Externals/Controllers/Authentication/signUpViewController.swift
+++ ADG-Externals/ADG-Externals/Controllers/Authentication/signUpViewController.swift
@@ -10,6 +10,7 @@ import Foundation
 
 class signUpViewController: UIViewController {
     
+    var token:[Any] = []
 
     //MARK:- ViewLifeCycle
     override func viewDidLoad() {
@@ -55,7 +56,7 @@ extension signUpViewController{
         if let url = URL(string: "https://adgrecruitments.herokuapp.com/user/signup"){
             var request = URLRequest(url: url)
             request.httpMethod = "POST"
-            request.setValue("application/json", forHTTPHeaderField: "auth-key")
+//            request.addValue("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmJmODQ0OGVmZWM4NzNjMzA5NTNjOGQiLCJpYXQiOjE2MDY3MzIzMDF9.ktJmM_bWMl_Qb9kkCi3zNq2uxo9-to4F4PZxYNSzL5c", forHTTPHeaderField: "auth-key")
             let parameters: [String : Any] = [
                 "name": fullName,
                 "regno": regNumber,
@@ -64,21 +65,20 @@ extension signUpViewController{
                 "yearofstudy": 1
             ]
 
-           // request.httpBody = parameters.percentEscaped().data(using: .utf8)
-            let postData = try! JSONSerialization.data(withJSONObject: parameters,options: [])
-            request.httpBody = postData
-            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-                guard let data = data else {
+           request.httpBody = parameters.percentEscaped().data(using: .utf8)
+//            let postData = try! JSONSerialization.data(withJSONObject: parameters,options: [])
+//            request.httpBody = postData
+            URLSession.shared.dataTask(with: request){(data, response, error) in
+                guard let data = data else{
                     if error == nil{
                         print(error?.localizedDescription ?? "Unknown Error")
                     }
                     return
                 }
-                
                 if let response = response as? HTTPURLResponse{
                     guard (200 ... 299) ~= response.statusCode else {
                         print("Status code :- \(response.statusCode)")
-                        print(response)
+                        //print(response)
                         return
                     }
                 }
@@ -86,14 +86,36 @@ extension signUpViewController{
                 do{
                     let json = try JSONSerialization.jsonObject(with: data, options: [])
                     print(json)
+//                    self.token.append(json)
+//                    print(self.token)
+//                    print(self.token[1])
+                    self.parseJSON(data)
                 }catch let error{
                     print(error.localizedDescription)
                 }
-            }
-            task.resume()
+            }.resume()
         }
     }
+    func parseJSON(_ data: Data){
+        
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(AuthKey.self, from: data)
+            
+            let key = decodedData.token
+            let message = decodedData.message
+          
+            token.append(key)
+            print(key)
+            print(message)
+            
+        }catch{
+            print("error!!!!")
+        }
+    }
+
 }
+
 extension Dictionary {
     func percentEscaped() -> String {
         return map { (key, value) in
---
+++ ADG-Externals/ADG-Externals/Model/Auth-Key.swift
@@ -0,0 +1,13 @@
+//
+//  Auth-Key.swift
+//  ADG-Externals
+//
+//  Created by PRABALJIT WALIA     on 07/12/20.
+//
+
+import Foundation
+struct AuthKey :Decodable {
+ 
+    let message: String
+    let token: String
+}
