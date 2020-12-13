1b95307a0efe27042610acf3d234a036e1990961
“gokulnair2001” <gokulnair.2001@gmail.com>
2020-12-13 11:34:20 +0530

    new updates

--- ADG-Externals/ADG-Externals/Controllers/QandA Section/GeneralTopicVC.swift
+++ ADG-Externals/ADG-Externals/Controllers/QandA Section/GeneralTopicVC.swift
@@ -11,8 +11,8 @@ class GeneralTopicVC: UIViewController, UITableViewDelegate, UITableViewDataSour
    
     @IBOutlet weak var tableView: UITableView!
     
-    var questionArray:[String] = ["Q1. How to start iOS App Developement"]
-    var answerArray:[String] = ["1.Get a Mac,2.Install Xcode, 3.Learn basics of Swift, 4.Watch online tutorials, 5.Try to make your custom app and work on it."]
+    var questionArray:[String] = ["Q1. What are the domains in ADG, is it only iOS Dev? Or What all departments are in the club?","Q2. How many events does ADG conduct in a year? And what kinds of events are they?","Q3. What does the chapter offer in terms of learning?"]
+    var answerArray:[String] = [" The following are the technical domains in ADG: IOS dev ,Android Dev, Web Dev, ML, Design, Competitive Coding (compulsory)","ADG usually conducts 2 events during both the technical and the cultural fests. One of these is the flagship event the 'iOS Fusion' where one gets to learn specifically about swift and its scope by getting some hands on practice during the workshop.","The chapter offers a lot of growth in the learning sphere so one can explore different domains as mentioned above and choose the ones they think are the best for them and build projects on them!"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- ADG-Externals/ADG-Externals/View/FAQRecruitmentViewController.swift
+++ ADG-Externals/ADG-Externals/View/FAQRecruitmentViewController.swift
@@ -11,8 +11,8 @@ class FAQRecruitmentViewController: UIViewController, UITableViewDelegate, UITab
 
     @IBOutlet weak var tableView: UITableView!
     
-    var questionArray:[String] = ["Q1. How to start iOS App Developement"]
-    var answerArray:[String] = ["1.Get a Mac,2.Install Xcode, 3.Learn basics of Swift, 4.Watch online tutorials, 5.Try to make your custom app and work on it."]
+    var questionArray:[String] = ["Q1. I want to join ADG. So…when do you recruit?","Q2. Do I need Apple devices such as a MacBook to be a part this chapter?","Q3. Hey, Do I need to be good in coding to be a part of this chapter?","Q4. Is it okay if I'm not from any computer science related branches?","Q5. Cool! I think I am ready to join ADG. But where do I apply for recruitment?"]
+    var answerArray:[String] = ["The tentative recruitment drive in at the start of every winter semester. Anything otherwise, shall be communicated through our social media platforms. Hence, keep checking them.","Its not at all necessary to have an apple device to be the part of ADG unless you need to be the part of iOS Domain, for iOS App Development Macbook is compulsory.","If you want to be a part of the technical domains then yes you need to have a good technical knowledge. However, we have 2 more domains apart from technical which are management and design. So, if you are interested in either of those then you can definitely apply!","Yes, it's completely okay! All you need is enthusiasm to learn and you're good to go!","The recruitment preliminary round will be a Quiz, conducted on the ADG-VIT website (adgvit.com) or App, qualified candidates will be contacted by ADG for further round(s) based on their Quiz score."]
     
     override func viewDidLoad() {
         super.viewDidLoad()
